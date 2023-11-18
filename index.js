50454d5f5a802f657e0f5cb7be0797a6e9c5e999
Mona <monabadee@gmail.com>
2023-11-18 21:49:10 +0300

    Complete  Lab

---
+++ index.js
@@ -0,0 +1,15 @@
+// Task 1
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  // Task 2
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Task 3
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
